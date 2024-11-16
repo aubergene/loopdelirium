8e7e2f2a9b19fc840b89e92b236aed2584b17a00
Julian Burgess <aubergene@gmail.com>
2024-11-16 16:53:58 +0000

    try to fix

--- svelte.config.js
+++ svelte.config.js
@@ -8,17 +8,18 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
 
-	adapter: adapter({
-		// default options are shown. On some platforms
-		// these options are set automatically — see below
-		pages: 'build',
-		assets: 'build',
-		fallback: undefined,
-		precompress: false,
-		strict: true,
-		fallback: '404.html'
-	}),
-
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true,
+			fallback: '404.html'
+		})
+	},
 	extensions: ['.svelte', '.svx']
 };
 
